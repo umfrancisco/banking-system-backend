6977c0ddc62a8a476dec994069cd3ebb64386489
umfrancisco <francis.guitler@gmail.com>
2026-06-26 08:25:02 -0300

    First commit

---
+++ src/main/java/com/umfrancisco/app/BankingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umfrancisco/app/BankingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
